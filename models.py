e02eb24f175e31368b9e0af8027e1d1a1ce9a31b
Akshaynath R <akshaynathr@gmail.com>
2017-03-24 00:47:39 +0530

    added new table 'reminder'

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ def dbSetUp():
         r.db_create('remember_bot').run(connection)
         r.db('remember_bot').table_create('user').run(connection)
         r.db('remember_bot').table_create('post').run(connection)
+        r.db('remember_bot').table_create('reminder').run(connection)
         print("Database setup completed")
     except RqlRuntimeError:
         print("Database running Okay")
